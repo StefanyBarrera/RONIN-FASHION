db081c0865fc58d6ea65a9d181021e38c73d926e
StefanyBarrera <63211739+StefanyBarrera@users.noreply.github.com>
2021-08-29 18:44:15 -0500

    segundo commit-declaración de variables del archivo config

---
+++ config/APP.php
@@ -0,0 +1,30 @@
+const SERVERURL= "htto://localhost/RONIN-FASHION/";
+const COMPANY="Ronin Fashion";
+const LANG="es";
+const DASHBOARD="admin";
+const SESSION_NAME="STO";
+const FACEBOOK="https://www.facebook.com/roninfashion";
+const INSTAGRAM="https://www.instagram.com/_roninfashion_/?hl=es";
+const YOUTUBE="";
+const TWITTER="";
+const COUNTRY="";
+const ADDRESS="Bogotá C.C Mundo del Oro";
+const COIN_SYMBOL="$";
+const COIN_NAME="USD";
+const COIN_DECIMALS="2";
+const COIN_SEPARATOR_THOUSAND=",";
+const COIN_SEPARATOR_DECIMAL=".";
+const DOCUMENTS_USERS=["DNI","Cedula","DUI","Licencia","Pasaporte","Otro"];
+const DOCUMENTS_COMPANY=["DNI","Cedula","RUT","NIT","RUC","Otro"];
+const PRODUTS_UNITS=["Unidad","Libra","Kilogramo","Caja","Paquete","Lata","Galon","Botella","Tira","Sobre","Bolsa","Saco","Tarjeta","Otro"];
+const COVER_PRODUCT=3;
+const GALLERY_PRODUCT=7;
+const FIELD_OBLIGATORY='&nbsp; <i class="fab fa-font-awesome-alt"></i> &nbsp;';
+const BARCODE_FORMAT="CODE128";
+const BARCODE_TEXT_ALIGN="center";
+const BARCODE_TEXT_POSITION="bottom";
+const THERMAL_PRINT_SIZE="80";
+
+
+
+	
