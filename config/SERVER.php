16225098308db32f532446807f269d1696ee9f84
StefanyBarrera <63211739+StefanyBarrera@users.noreply.github.com>
2021-08-29 18:57:59 -0500

    cuarto commit-declaración de variables del archivo config y anexo de imágenes

--- config/SERVER.php
+++ config/SERVER.php
@@ -2,3 +2,7 @@ const SERVER="localhost:3310";
 const DB="tienda";
 const USER="root";
 const PASS="";
+const SGBD="mysql:host=".SERVER.";dbname=".DB;
+const METHOD="AES-256-CBC";
+const SECRET_KEY='$STO@2021';
+const SECRET_IV='102791';
