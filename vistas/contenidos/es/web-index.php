e58add1174733a58873dfed9a1e5df323725f3de
StefanyBarrera <63211739+StefanyBarrera@users.noreply.github.com>
2021-08-29 19:11:58 -0500

    sexto commit-creación y codificación de archivo web-index.php, para el módulo de inicio

---
+++ vistas/contenidos/es/web-index.php
@@ -0,0 +1,21 @@
+
+
+<div class="container container-web-page">
+    <div class="row justify-content-md-center">
+        <div class="col-12 col-md-6">
+            <figure class="full-box">
+                <img src="<?php echo SERVERURL; ?>vistas/assets/img/95946292_120507859640175_1940188554697113600_n.jpg" alt="registration_<?php echo COMPANY; ?>" class="img-fluid">
+            </figure>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-12 col-md-6">
+            <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Crea tu cuenta</h3>
+            <p class="text-justify">
+                Crea tu cuenta para poder agregar los productos que mas te gusten.
+            </p>
+            <p class="text-center">
+                <a href="<?php echo SERVERURL; ?>registration/" class="btn btn-primary" >Crear cuenta</a>
+            </p>
+        </div>
+    </div>
+</div>
