736836705a49c55f30ecbf2ead5fe24421e4e78a
StefanyBarrera <63211739+StefanyBarrera@users.noreply.github.com>
2021-08-29 19:57:03 -0500

    commit número 9- cofiguración final de archivos

--- config/APP.php
+++ config/APP.php
@@ -1,30 +1,94 @@
-const SERVERURL= "htto://localhost/RONIN-FASHION/";
-const COMPANY="Ronin Fashion";
-const LANG="es";
-const DASHBOARD="admin";
-const SESSION_NAME="STO";
-const FACEBOOK="https://www.facebook.com/roninfashion";
-const INSTAGRAM="https://www.instagram.com/_roninfashion_/?hl=es";
-const YOUTUBE="";
-const TWITTER="";
-const COUNTRY="";
-const ADDRESS="Bogotá C.C Mundo del Oro";
-const COIN_SYMBOL="$";
-const COIN_NAME="USD";
-const COIN_DECIMALS="2";
-const COIN_SEPARATOR_THOUSAND=",";
-const COIN_SEPARATOR_DECIMAL=".";
-const DOCUMENTS_USERS=["DNI","Cedula","DUI","Licencia","Pasaporte","Otro"];
-const DOCUMENTS_COMPANY=["DNI","Cedula","RUT","NIT","RUC","Otro"];
-const PRODUTS_UNITS=["Unidad","Libra","Kilogramo","Caja","Paquete","Lata","Galon","Botella","Tira","Sobre","Bolsa","Saco","Tarjeta","Otro"];
-const COVER_PRODUCT=3;
-const GALLERY_PRODUCT=7;
-const FIELD_OBLIGATORY='&nbsp; <i class="fab fa-font-awesome-alt"></i> &nbsp;';
-const BARCODE_FORMAT="CODE128";
-const BARCODE_TEXT_ALIGN="center";
-const BARCODE_TEXT_POSITION="bottom";
-const THERMAL_PRINT_SIZE="80";
+<?php
+
+	/*----------  
+	Ruta o dominio del servidor  - Server path or domain
+	----------*/
+	const SERVERURL="http://localhost/RONIN-FASHION/";
 
 
+	/*----------  
+	Nombre de la empresa o compañia -  Company or company name
+	----------*/
+	const COMPANY="Ronin Fashion";
+
+
+	/*----------  Idioma - Language
+	Español -> es 
+	----------*/
+	const LANG="es";
 
 	
+	/*----------  
+		Palabra clave dashboard - Dashboard keyword
+		No usar los siguientes valores - Do not use the following values
+
+		index | product | bag | registration | details | signin
+	----------*/
+	const DASHBOARD="admin";
+
+
+	/*----------  
+	Nombre de la sesion -  Session name
+	----------*/
+	const SESSION_NAME="STO";
+
+
+	/*----------  Redes sociales - Social networks  ----------*/
+	const FACEBOOK="https://www.facebook.com/roninfashion";
+	const INSTAGRAM="https://www.instagram.com/_roninfashion_/?hl=es";
+	const YOUTUBE="";
+	const TWITTER="";
+
+
+	/*----------  Direccion - Address  ----------*/
+	const COUNTRY="";
+	const ADDRESS="Bogotá C.C Mundo del Oro";
+	
+
+	/*----------  Configuración de moneda - Currency Settings  ----------*/
+	const COIN_SYMBOL="$";
+	const COIN_NAME="USD";
+	const COIN_DECIMALS="2";
+	const COIN_SEPARATOR_THOUSAND=",";
+	const COIN_SEPARATOR_DECIMAL=".";
+
+
+	/*----------  Tipos de documentos - Document types ----------*/
+	const DOCUMENTS_USERS=["DNI","Cedula","DUI","Licencia","Pasaporte","Otro"];
+	const DOCUMENTS_COMPANY=["DNI","Cedula","RUT","NIT","RUC","Otro"];
+
+
+	/*----------  Tipos de unidades de productos - Types of product units ----------*/
+	const PRODUTS_UNITS=["Unidad","Libra","Kilogramo","Caja","Paquete","Lata","Galon","Botella","Tira","Sobre","Bolsa","Saco","Tarjeta","Otro"];
+
+	/*----------  Límite de tamaño de imágenes de productos en MB - Product image size limit in MB ----------*/
+	const COVER_PRODUCT=3;
+	const GALLERY_PRODUCT=7;
+
+
+	/*----------  Marcador de campos obligatorios - Mandatory field marker  ----------*/
+	const FIELD_OBLIGATORY='&nbsp; <i class="fab fa-font-awesome-alt"></i> &nbsp;';
+
+
+	/*----------  Configuración de codigos de barras - Bar code settings
+
+		BARCODE_FORMAT -> CODE128 | CODE39 | EAN | EAN-13 | EAN-8 | EAN-5 | EAN-2 | UPC | ITF | ITF-14 | MSI | MSI10 | MSI11 | MSI1010 | MSI1110 | Pharmacode
+
+		BARCODE_TEXT_ALIGN -> center | left | right
+
+		BARCODE_TEXT_POSITION -> top | bottom
+
+	----------*/
+
+	const BARCODE_FORMAT="CODE128";
+	const BARCODE_TEXT_ALIGN="center";
+	const BARCODE_TEXT_POSITION="bottom";
+
+
+	/*----------  Tamaño de papel de impresora termica (en milimetros) - Thermal printer paper size (in millimeters)
+		THERMAL_PRINT_SIZE -> 80 | 57
+	----------*/
+	const THERMAL_PRINT_SIZE="80";
+
+
+
