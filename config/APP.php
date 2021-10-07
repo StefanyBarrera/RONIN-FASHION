a62ce09a97654617fbd8d6f841dac3253bb29b4f
StefanyBarrera <63211739+StefanyBarrera@users.noreply.github.com>
2021-10-06 22:26:03 -0500

    deploy heroku

--- config/APP.php
+++ config/APP.php
@@ -3,7 +3,7 @@
 	/*----------  
 	Ruta o dominio del servidor  - Server path or domain
 	----------*/
-	const SERVERURL="http://localhost/RONIN-FASHION/";
+	const SERVERURL="http://ca72-186-119-197-129.ngrok.io/RONIN-FASHION/";
 
 
 	/*----------  
