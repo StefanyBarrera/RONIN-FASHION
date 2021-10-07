7e2c3a99bae5cb0a7feac8e78b54906a81f980a6
StefanyBarrera <63211739+StefanyBarrera@users.noreply.github.com>
2021-10-06 23:33:57 -0500

    dominio

--- config/APP.php
+++ config/APP.php
@@ -3,7 +3,7 @@
 	/*----------  
 	Ruta o dominio del servidor  - Server path or domain
 	----------*/
-	const SERVERURL="http://ca72-186-119-197-129.ngrok.io/RONIN-FASHION/";
+	const SERVERURL="https://quiet-basin-25086.herokuapp.com/RONIN-FASHION/";
 
 
 	/*----------  
